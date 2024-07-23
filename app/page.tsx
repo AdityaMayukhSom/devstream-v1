d99ef368df4d42100cbc2d3b61c37938d5f1eaa7
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-07-24 02:25:21 +0530

    [ADDED] Mandelbrot Set Generator

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,87 @@
-import Image from "next/image";
+'use client'
+
+import { useEffect, useRef } from "react"
+
+class Complex {
+  public x: number
+  public y: number
+
+  constructor(x: number, y: number) {
+    this.x = x
+    this.y = y
+  }
+}
+
+class Mandelbrot {
+  private static MAX_ITERATION = 50
+  private static PIXEL_SKIP_SIZE = 6
+  private static PIXEL_CLUSTER_SIZE = 3
+
+  private static BACKGROUND_COLOR_HEX = '#fda403'
+  private static FOREGROUND_COLOR_HEX = '#fff'
+
+  private static REAL_SET = { "start": -2, "end": 1 }
+  private static IMAG_SET = { "start": -1, "end": 1 }
+
+
+  private generateRandom24BitColor() {
+    return `#${((1 << 24) * Math.random() | 0).toString(16)}`
+  }
+
+  private isStable(real: number, imag: number, maxIteration: number = Mandelbrot.MAX_ITERATION): boolean {
+    const c = new Complex(real, imag)
+    const z = new Complex(0, 0)
+    const z2pc = new Complex(0, 0)
+    for (let index = 0, dist = 0; index < maxIteration; index++) {
+      z2pc.x = (z.x * z.x) - (z.y * z.y) + c.x
+      z2pc.y = 2 * z.x * z.y + c.y
+      z.x = z2pc.x
+      z.y = z2pc.y
+      dist = z.x * z.x + z.y * z.y
+      if (dist > 4) {
+        return false
+      }
+    }
+    return true
+  }
+
+  public draw(canvas: HTMLCanvasElement, canvasHeight: number = 400, canvasWidth: number = 600) {
+    const ctx = canvas.getContext("2d")
+    if (!ctx) {
+      console.error("could not find context to draw mandelbrot plot")
+      return
+    }
+
+    ctx.canvas.width = canvasWidth
+    ctx.canvas.height = canvasHeight
+
+    for (let i = 0, halfCanvasHeight = Math.ceil(canvasHeight / 2); i < canvasWidth; i += Mandelbrot.PIXEL_SKIP_SIZE) {
+      for (let j = 0; j <= halfCanvasHeight; j += Mandelbrot.PIXEL_SKIP_SIZE) {
+        const real = Mandelbrot.REAL_SET.start + (i * (Mandelbrot.REAL_SET.end - Mandelbrot.REAL_SET.start)) / canvasWidth
+        const imag = Mandelbrot.IMAG_SET.start + (j * (Mandelbrot.IMAG_SET.end - Mandelbrot.IMAG_SET.start)) / canvasHeight
+        const inMandelbrot = this.isStable(real, imag)
+        ctx.fillStyle = inMandelbrot ? Mandelbrot.BACKGROUND_COLOR_HEX : Mandelbrot.FOREGROUND_COLOR_HEX
+        ctx.fillRect(i, j, Mandelbrot.PIXEL_CLUSTER_SIZE, Mandelbrot.PIXEL_CLUSTER_SIZE)
+        ctx.fillRect(i, canvasHeight - j, Mandelbrot.PIXEL_CLUSTER_SIZE, Mandelbrot.PIXEL_CLUSTER_SIZE)
+      }
+    }
+  }
+}
+
 
 export default function Home() {
+  const mandelbrotCanvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (mandelbrotCanvasRef.current) {
+      const mandelbrot = new Mandelbrot()
+      mandelbrot.draw(mandelbrotCanvasRef.current, window.innerHeight, window.innerWidth * 0.85)
+    }
+  }, [])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="">
+      <canvas ref={mandelbrotCanvasRef} className="m-auto" />
     </main>
   );
 }
