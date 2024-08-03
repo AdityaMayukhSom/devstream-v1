2174ae1af13e4e6961cfa7818fe041a4a7969bf0
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-08-04 00:02:54 +0530

    [ADD]: ui rendering function for blogs

--- app/algorithms/[...slug]/page.tsx
+++ app/algorithms/[...slug]/page.tsx
@@ -33,3 +33,41 @@ export function generateStaticParams() {
 
     return files.map((file) => file.toString().replace(".mdx", "").split("/"));
 }
+
+interface Params {
+    params: {
+        slug: string[];
+    };
+}
+
+export const metadata: Metadata = {};
+
+export default async function AlgorithmsPage({ params }: Params) {
+    const components = useMDXComponents({});
+    const sourcePath = path.join(process.cwd(), contentSource, params.slug.join("/")) + ".mdx";
+    if (!fs.existsSync(sourcePath)) {
+        notFound();
+    }
+
+    const sourceStats = fs.statSync(sourcePath);
+
+    if (!sourceStats.isFile()) {
+        throw new Error(`${params.slug.at(params.slug.length - 1)} is not a valid file.`);
+    }
+
+    const source = fs.readFileSync(sourcePath, "utf-8");
+
+    const { content, frontmatter } = await compileMDX({
+        source,
+        options: {
+            mdxOptions: {
+                remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+                rehypePlugins: [rehypeHighlight, rehypeHighlightCodeLines],
+            },
+            parseFrontmatter: true,
+        },
+        components,
+    });
+
+    return <article className="pt-36 pb-32 ml-60 w-1/2">{content}</article>;
+}
