4771a0f97c7245ff1d59069c1baf91563fe51769
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-08-03 17:00:57 +0530

    [ADD]: class to handle computation of whether a point belongs to mandelbrot set or not

---
+++ classes/mandelbrot_compute.ts
@@ -0,0 +1,25 @@
+import Complex from "@classes/complex";
+
+class MandelbrotCompute {
+    private static readonly MAX_ITERATION = 40;
+
+    public static isStable(real: number, imag: number, maxIters = MandelbrotCompute.MAX_ITERATION): boolean {
+        const c = new Complex(real, imag);
+        const z = new Complex(0, 0);
+
+        for (let index = 0, dist = 0, z2pc = new Complex(0, 0); index < maxIters; index++) {
+            z2pc.x = z.x * z.x - z.y * z.y + c.x;
+            z2pc.y = 2 * z.x * z.y + c.y;
+            z.x = z2pc.x;
+            z.y = z2pc.y;
+            dist = z.x * z.x + z.y * z.y;
+            if (dist > 4) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+export default MandelbrotCompute;
