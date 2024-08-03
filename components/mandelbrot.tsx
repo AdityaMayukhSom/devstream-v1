25a3e1a7316ecd4b1374d4b3bb9c0fc2980f1751
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-08-03 17:03:12 +0530

    [ADD]: base component to render mandelbrot set

---
+++ components/mandelbrot.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useEffect, useMemo, useRef } from "react";
+import Dimention from "@classes/dimention";
+import MandelbrotRenderer from "@/classes/mandelbrot_renderer";
+
+const MandelbrotComponent = (props: { dimention: Dimention }) => {
+    const mandelbrotCanvasRef = useRef<HTMLCanvasElement>(null);
+    const mandelbrotRenderer = useMemo(() => new MandelbrotRenderer(), []);
+
+    useEffect(() => {
+        if (mandelbrotCanvasRef.current) {
+            let { width, height } = props.dimention;
+
+            if (2 * width < 3 * height) {
+                height = 0.66 * width;
+            } else {
+                width = 1.5 * height;
+            }
+
+            mandelbrotRenderer.draw(mandelbrotCanvasRef.current, width, height);
+        }
+
+        return () => {
+            mandelbrotRenderer.release();
+        };
+    }, [props.dimention, mandelbrotRenderer]);
+
+    return <canvas ref={mandelbrotCanvasRef} className="m-auto" />;
+};
+
+export default MandelbrotComponent;
