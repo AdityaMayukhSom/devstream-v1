7b1e22e1179833555e7c5f98e2f078ff303bf767
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-08-05 03:59:36 +0530

    [PSEUDO]: ramdom base route ui updates

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
-    const name = "Aditya";
-    return <main>Hola {name}</main>;
+    const name = "Aditya Mayukh Som";
+    return <main className="min-h-svh w-full text-center">Website Maintainer: {name}</main>;
 };
 
 export default Home;
