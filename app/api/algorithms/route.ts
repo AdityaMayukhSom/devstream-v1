0f1c2a4ea8b41323e8614b694f9aaeb34fd3f6bc
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-08-04 16:24:54 +0530

    [ADD]: api handler to return article names

--- app/api/algorithms/route.ts
+++ app/api/algorithms/route.ts
@@ -5,25 +5,10 @@ import { CONTENT_SRC_PATH } from "@lib/constants";
 export const runtime = "nodejs";
 export const dynamic = "error";
 
-function throughDirectory(directory: string, fileList: Array<String>): void {
-    const directoryPath = path.resolve(directory);
-
-    if (!fs.existsSync(directoryPath)) return;
-    if (!fs.statSync(directoryPath).isDirectory()) return;
-
-    const directoryContents = fs.readdirSync(directoryPath, {
-        encoding: "utf-8",
-        withFileTypes: true,
-    });
-
-    for (const content of directoryContents) {
-        const absolutePath = path.join(directory, content.name);
-        const fileStatistics = fs.statSync(absolutePath);
-        if (fileStatistics.isFile() && content.name.endsWith(".mdx")) {
-            fileList.push(content.name.split(".")[0]);
-        } else if (fileStatistics.isDirectory()) {
-            throughDirectory(absolutePath, fileList);
-        }
-    }
+export async function GET(request: Request) {
+    const articlesDirectory = path.join(process.cwd(), CONTENT_SRC_PATH);
+    const fileNames = fs.readdirSync(articlesDirectory);
+    const fileList = fileNames.map((fileName) => fileName.replace(/\.mdx$/, ""));
+    const payload = { fileList };
+    return Response.json(payload, { status: 200 });
 }
-
