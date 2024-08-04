e2d3972c2a9660f071d167dd03997fa1483bb864
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-08-04 11:15:33 +0530

    [ADD]: anchor link tag and list item styling

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,25 +1,28 @@
 import type { MDXComponents } from "mdx/types";
+import Link from "next/link";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
     const singleWhiteSpace = " ";
     return {
         ul: ({ children }) => <ul className="pb-4">{children}</ul>,
         li: ({ children }) => (
-            <li className="article-list-item grid grid-cols-[auto_1fr] before:content-['—'] before:text-yellow-400 before:pr-4">
-                {children}
+            <li className="grid grid-cols-[auto_1fr] before:content-['—'] before:leading-snug before:text-yellow-400 before:pr-4">
+                <div>{children}</div>
             </li>
         ),
         p: ({ children, ...rest }) => (
-            <p className="pb-4 font-normal text-left prose-base" {...rest}>
+            <p className="pb-3 font-normal" {...rest}>
                 {children}
             </p>
         ),
-        h1: ({ children }) => <h1 className="text-4xl font-semibold py-4 mb-4">{children}</h1>,
-        h2: ({ children }) => <h2 className="text-[28px] leading-4 font-semibold py-3 mb-2">{children}</h2>,
-        h3: ({ children }) => <h3 className="text-[20px] leading-4 font-semibold py-2">{children}</h3>,
+        h1: ({ children }) => (
+            <h1 className="text-4xl font-semibold py-4 mb-4 selection:bg-sky-200 selection:text-sky-900">{children}</h1>
+        ),
+        h2: ({ children }) => <h2 className="text-[27px] font-semibold pb-4 pt-3">{children}</h2>,
+        h3: ({ children }) => <h3 className="text-xl font-semibold pb-3 pt-1">{children}</h3>,
         pre: ({ children, ...rest }) => (
             <pre
-                className="p-3 my-5 text-base leading-none *:selection:bg-emerald-200 *:selection:text-emerald-900"
+                className="p-3 mb-5 rounded-sm leading-none *:selection:bg-emerald-200 *:selection:text-emerald-900 overflow-hidden"
                 {...rest}
             >
                 {children}
@@ -30,7 +33,7 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
                 className={
                     (className ?? "") +
                     singleWhiteSpace +
-                    "font-monospace-only text-base leading-none *:font-monospace-only *:leading-none *:text-base line-number:content-[attr(data-line-number)] line-number:inline-block line-number:w-3 line-number:mr-4 line-number:text-right line-number:text-gray-600 line-number:leading-none line-number:text-base"
+                    "font-monospace-only *:leading-none [&:not(.hljs)]:text-yellow-300 line-number:content-[attr(data-line-number)] line-number:inline-block line-number:w-5 line-number:mr-4 line-number:text-right line-number:text-gray-600 line-number:leading-none"
                 }
                 {...rest}
             >
@@ -38,9 +41,13 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
             </code>
         ),
         a: ({ children, href, ...rest }) => (
-            <a href={href} {...rest}>
+            <Link
+                href={href ?? ""}
+                className="border-spacing-1 border-b-2 transition-all ease-in-out duration-150 border-gray-500 hover:border-gray-100"
+                {...rest}
+            >
                 {children}
-            </a>
+            </Link>
         ),
         ...components,
     };
