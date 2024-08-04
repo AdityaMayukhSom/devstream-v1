1145cf8fcbbe2272d215c05c2386b8f87721b052
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-08-05 02:45:49 +0530

    [ADD]: mdx plugins for nominal page.mdx

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 import createMDX from "@next/mdx";
 
+import remarkGfm from "remark-gfm";
+import rehypeSanitize from "rehype-sanitize";
+import rehypeHighlight from "rehype-highlight";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkMdxFrontmatter from "remark-mdx-frontmatter";
+import rehypeHighlightCodeLines from "rehype-highlight-code-lines";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
@@ -7,5 +14,11 @@ const nextConfig = {
     experimental: {},
 };
 
-const withMDX = createMDX();
+const withMDX = createMDX({
+    options: {
+        remarkPlugins: [remarkGfm, remarkFrontmatter, remarkMdxFrontmatter],
+        rehypePlugins: [rehypeSanitize, rehypeHighlight, rehypeHighlightCodeLines],
+    },
+});
+
 export default withMDX(nextConfig);
