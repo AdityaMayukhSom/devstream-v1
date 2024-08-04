5aa9a7a183f70ca82f9322b2238cc62adc372b46
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-08-04 18:28:01 +0530

    [REMOVE]: layout file for algorithms listing

--- app/algorithms/layout.tsx
+++
@@ -1,5 +0,0 @@
-const AlgorithmsListLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
-    return <main className="bg-dark-base text-white min-h-svh text-sm">{children}</main>;
-};
-
-export default AlgorithmsListLayout;
