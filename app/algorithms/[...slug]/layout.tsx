cbea5710db5d8d94e486997b39ed490faf6f2814
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-08-04 09:45:08 +0530

    [UPDATE]: default font stays as Inter

--- app/algorithms/[...slug]/layout.tsx
+++ app/algorithms/[...slug]/layout.tsx
@@ -1,10 +1,3 @@
-import { Roboto } from "next/font/google";
-
-const roboto = Roboto({
-    subsets: ["latin"],
-    weight: ["100", "300", "400", "500", "700", "900"],
-});
-
 export default function AlgorithmsLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-    return <main className={roboto.className}>{children}</main>;
+    return <main className="bg-dark-base text-white">{children}</main>;
 }
