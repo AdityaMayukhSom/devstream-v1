75d88174f69fdb5565c504715525714131296cb0
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-08-24 11:31:19 +0530

    [FIX]: footer does not parallax

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,14 +7,36 @@ import Footer from "./footer";
 
 const Home = () => {
     return (
-        <main className="min-h-svh relative w-full grid grid-cols-[auto_1fr] bg-nearly-black text-xl *:selection:bg-emerald-200 *:selection:text-emerald-900">
+        <main className="min-h-svh relative w-full grid grid-cols-[auto_1fr] text-xl *:selection:bg-emerald-200 *:selection:text-emerald-900 text-white/[0.87] scroll-smooth">
             <Navbar />
             <section>
                 <Hero />
-                <Analytics />
+                <Analytics title="1&emsp;What We Do" id="what-we-do">
+                    We are dedicated to empowering businesses by providing a wide range of services. We focus on
+                    software development, offering our expertise in creating robust applications. We also provide
+                    consulting services, helping teams optimize their workflows and adopt the best practices in the
+                    industry. Our goal is to foster a thriving community where developers can learn, grow, and
+                    collaborate effectively.
+                </Analytics>
+                <Analytics title="2&emsp;Who We Are" id="who-we-are" dark>
+                    We are dedicated to empowering businesses by providing a wide range of services. We focus on
+                    software development, offering our expertise in creating robust applications. We also provide
+                    consulting services, helping teams optimize their workflows and adopt the best practices in the
+                    industry. Our goal is to foster a thriving community where developers can learn, grow, and
+                    collaborate effectively.
+                </Analytics>
+                <section className="sticky">
+                    <Analytics title="3&emsp;How To Join" id="how-to-join" sticky={false}>
+                        We are dedicated to empowering businesses by providing a wide range of services. We focus on
+                        software development, offering our expertise in creating robust applications. We also provide
+                        consulting services, helping teams optimize their workflows and adopt the best practices in the
+                        industry. Our goal is to foster a thriving community where developers can learn, grow, and
+                        collaborate effectively.
+                    </Analytics>
+                    <Footer />
+                </section>
                 {/* <Newsletter /> */}
                 {/* <Cards /> */}
-                <Footer />
             </section>
         </main>
     );
